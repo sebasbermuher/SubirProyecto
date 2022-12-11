f44d44266d529d05fd851b204ee45470b6a37562
Sebastian Bermudez Herrera <alum.sbermudezh@iesalixar.org>
2022-12-11 11:29:33 +0100

    Actualizacion

--- src/main/java/org/iesalixar/servidor/controller/MyErrorController.java
+++ src/main/java/org/iesalixar/servidor/controller/MyErrorController.java
@@ -34,6 +34,8 @@ public class MyErrorController implements ErrorController {
 				return "error-404";
 			} else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
 				return "error-500";
+			} else if (statusCode == HttpStatus.FORBIDDEN.value()) {
+				return "forbidden";
 			}
 		}
 		return "error";
